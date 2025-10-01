d85426d336c4d156282cabfea1cc935cd2c21ed6
Sebastian Suarez-Solis <developer@stagedevices.com>
2025-10-01 11:02:08 -0700

    Initial Commit

---
+++ Tenney/TenneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenneyApp.swift
+//  Tenney
+//
+//  Created by Sebastian Suarez-Solis on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TenneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TenneyTests/TenneyTests.swift
@@ -0,0 +1,16 @@
+//
+//  TenneyTests.swift
+//  TenneyTests
+//
+//  Created by Sebastian Suarez-Solis on 10/1/25.
+//
+
+import Testing
+
+struct TenneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
