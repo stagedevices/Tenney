92569d1b4ba90dbc4ea24c6e4d42037dda08cc4f
seb suarez <162302050+cbassuarez@users.noreply.github.com>
2026-01-17 18:32:55 -0800

    Update TenneyScaleFixtureTest.kt

--- apps/android/core/src/test/kotlin/com/tenney/core/TenneyScaleFixtureTest.kt
+++ apps/android/core/src/test/kotlin/com/tenney/core/TenneyScaleFixtureTest.kt
@@ -1,5 +1,6 @@
 package com.tenney.core
 
+import kotlin.math.min
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
@@ -32,7 +33,9 @@ class TenneyScaleFixtureTest {
             val inputHasId = (case.input as? JsonObject)?.containsKey("id") == true
             val expected = if (inputHasId) case.expected else case.expected.copy(id = actual.id)
             if (expected != actual) {
-                throw AssertionError("case=${case.id}\nexpected=$expected\nactual=$actual")
+                 val msg = buildScaleDiff(case.id, expected, actual)
+                println(msg)
+                throw AssertionError(msg)
             }
         }
     }
@@ -46,8 +49,46 @@ class TenneyScaleFixtureTest {
             val inputHasId = (case.input as? JsonObject)?.containsKey("id") == true
             val expected = if (inputHasId) case.expected else case.expected.copy(id = actual.id)
             if (expected != actual) {
-                throw AssertionError("case=${case.id}\nexpected=$expected\nactual=$actual")
+                val msg = buildScaleDiff(case.id, expected, actual)
+                println(msg)
+                throw AssertionError(msg)
             }
+        }
+    }
+
+    private fun buildScaleDiff(caseId: String, expected: TenneyScale, actual: TenneyScale): String {
+        val diffs = mutableListOf<String>()
+        fun diff(field: String, e: Any?, a: Any?) {
+            if (e != a) diffs += "$field\n  expected=$e\n  actual=$a"
+        }
+        diff("id", expected.id, actual.id)
+        diff("name", expected.name, actual.name)
+        diff("descriptionText", expected.descriptionText, actual.descriptionText)
+        diff("tags", expected.tags, actual.tags)
+        diff("favorite", expected.favorite, actual.favorite)
+        diff("lastPlayed", expected.lastPlayed, actual.lastPlayed)
+        diff("referenceHz", expected.referenceHz, actual.referenceHz)
+        diff("rootLabel", expected.rootLabel, actual.rootLabel)
+        diff("periodRatio", expected.periodRatio, actual.periodRatio)
+        diff("detectedLimit", expected.detectedLimit, actual.detectedLimit)
+        diff("maxTenneyHeight", expected.maxTenneyHeight, actual.maxTenneyHeight)
+        diff("author", expected.author, actual.author)
+
+        if (expected.degrees != actual.degrees) {
+            diffs += "degrees (size expected=${expected.degrees.size} actual=${actual.degrees.size})"
+            val n = min(expected.degrees.size, actual.degrees.size)
+            for (i in 0 until n) {
+                if (expected.degrees[i] != actual.degrees[i]) {
+                    diffs += "degrees[$i]\n  expected=${expected.degrees[i]}\n  actual=${actual.degrees[i]}"
+                    break
+                }
+            }
+        }
+
+        return buildString {
+            appendLine("case=$caseId")
+            appendLine("diffs=${diffs.size}")
+            diffs.forEach { appendLine(it) }
         }
     }
 }
