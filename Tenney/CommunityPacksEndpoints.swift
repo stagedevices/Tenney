a8e2fe09bcfa6e1e46f9a62444ea6cf1e7dde15c
Stage Devices <developer@stagedevices.com>
2026-01-19 15:12:47 -0800

    Fix community pack fetch fallback

--- Tenney/CommunityPacksEndpoints.swift
+++ Tenney/CommunityPacksEndpoints.swift
@@ -7,4 +7,12 @@ enum CommunityPacksEndpoints {
     static let indexPath = "INDEX.json"
     static let submitURL = URL(string: "https://tenneyapp.com/community")!
     static let issuesURL = URL(string: "https://github.com/stagedevices/tenney-scales/issues/new?template=pack_submission.yml")!
+
+    static func url(base: URL, path: String) -> URL {
+        var url = base
+        for component in path.split(separator: "/") {
+            url.appendPathComponent(String(component))
+        }
+        return url
+    }
 }
